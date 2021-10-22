389fe9b1d5ed56fa153678b18ad65fdad05dd690
MCTzOCK <dev@mctzock.de>
2021-10-22 19:56:24 +0200

    Added something

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
 app.use(cors());
 app.use(bodyParser());
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,18 @@
  * @copyright (c) 2018-2021 Craftions.net. All rights reserved.
  */
 
-import * as React from 'react';
+import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Editor from "./compoenents/Editor/Editor";
 
-ReactDOM.render((
-    <h1>Test</h1>
-), document.querySelector("#root"))
+ReactDOM.render(
+  <>
+    <BrowserRouter>
+      <Switch>
+        <Route path={"/editor"} exact component={Editor} />
+      </Switch>
+    </BrowserRouter>
+  </>,
+  document.querySelector("#root")
+);
